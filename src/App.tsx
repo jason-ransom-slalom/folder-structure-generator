1cd96da4bc239418ea8bc555da9cd22499637533
Vilasini_Sarveswara <vilasini.sarveswara@slalom.com>
2022-09-09 13:20:53 -0400

    Root folder and child folders have the level description at the end of the breadcrumb

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ export default function App() {
   return (
     <div>
       <div className="header">
-        <Breadcrumbs className="breadcrumbs" inputItems={["folder1", "folder2", "file1.txt"]} />
+        <Breadcrumbs
+          className="breadcrumbs"
+          inputItems={["folder1", "folder2", "folder3", "file1.txt"]}
+        />
+        <Breadcrumbs className="breadcrumbs" inputItems={["folder1"]} />
+        <Breadcrumbs
+          className="breadcrumbs"
+          inputItems={["folder1", "folder2", "folder3", "folder4"]}
+        />
       </div>
       <div className="body">
         <Folder className="folder" />
--- src/components/Breadcrumbs/__tests__/breadcrumbsUtil.test.ts
+++ src/components/Breadcrumbs/__tests__/breadcrumbsUtil.test.ts
@@ -1,19 +1,45 @@
-import { formatBreadcrumbs } from '../breadcrumbsUtil';
+import { formatBreadcrumbs } from "../breadcrumbsUtil";
 
-describe('formatBreadcrumb', () => {
-    it('is as function', () => {
-        expect(typeof formatBreadcrumbs).toBe('function');
-    });
+describe("formatBreadcrumb", () => {
+  it("is as function", () => {
+    expect(typeof formatBreadcrumbs).toBe("function");
+  });
 
-    it('returns each string in uppercase', () => {
-        const lowerCaseStrings = ['a', 'b', 'c'];
+  it("returns each string in uppercase", () => {
+    const lowerCaseStrings = ["a", "b", "c"];
 
-        expect(formatBreadcrumbs(lowerCaseStrings)).toMatchObject(['A', 'B', 'C']);
-    });
+    expect(formatBreadcrumbs(lowerCaseStrings)).toMatchObject([
+      "A",
+      "B",
+      "C (level 2 folder)",
+    ]);
+  });
 
-    it('returns extensions in lowercase', () => {
-        const lowerCaseStrings = ['a', 'b.txt', 'c.a.ts'];
+  it("returns extensions in lowercase", () => {
+    const lowerCaseStrings = ["a", "b.txt", "c.a.ts"];
 
-        expect(formatBreadcrumbs(lowerCaseStrings)).toMatchObject(['A', 'B.txt', 'C.A.ts']);
-    });
+    expect(formatBreadcrumbs(lowerCaseStrings)).toMatchObject([
+      "A",
+      "B.txt",
+      "C.A.ts",
+    ]);
+  });
+
+  it("has '(root folder) appended if the user is in the root folder' ", () => {
+    const lowerCaseStrings = ["TestFolder"];
+
+    expect(formatBreadcrumbs(lowerCaseStrings)).toMatchObject([
+      "TESTFOLDER (root folder) ",
+    ]);
+  });
+
+  it("displays the folder level at the end of the breadcrumb when nested ", () => {
+    const lowerCaseStrings = ["TestFolder1", "TestFolder2", "TestFolder3"];
+
+    expect(formatBreadcrumbs(lowerCaseStrings)).toMatchObject([
+      "TESTFOLDER1",
+      "TESTFOLDER2",
+      "TESTFOLDER3 (level 2 folder)",
+    ]);
+  });
 });
